522c4156fc678bea17fe7a39f4af956df2be0442
emmanuel <emmanuelpayares.idetp@gmail.com>
2023-02-19 09:24:17 -0800

    correction 6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - Prints a message
  * @void: No parameter
  *
- * return - Always 0 (Success)
+ * Return - Always 0 (Success)
  */
-void main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
