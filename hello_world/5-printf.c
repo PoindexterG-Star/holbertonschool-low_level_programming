4ee8baa06fe67f302b5b9f2c5d81037e9e074ca8
emmanuel <emmanuelpayares.idetp@gmail.com>
2023-02-19 09:38:54 -0800

    Steps for task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a message
+ *
+ * Return: Always a zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
