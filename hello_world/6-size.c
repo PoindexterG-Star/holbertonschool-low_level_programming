a523dc5fad65ec57ee7c26de2046ebc713d6dfa7
emmanuel <emmanuelpayares.idetp@gmail.com>
2023-02-19 10:47:42 -0800

    Correction 1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu ", sizeof(char), "byte(s)");
-	printf("Size of an int: %lu ", sizeof(int), "byte(s)");
-	printf("Size of a long int: %lu ", sizeof(long int), "byte(s)");
-	printf("Size of a long long int: %lu ", sizeof(long long int), "byte(s)");
-	printf("Size of float: %lu ", sizeof(float), "byte(s)");
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of float: %lu byte(s)", sizeof(float));
 	return (0);
 }
