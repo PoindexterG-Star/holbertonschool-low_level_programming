4c322a8cb003e27ff8eb26dc981fd50c54f350e4
emmanuel <emmanuelpayares.idetp@gmail.com>
2023-02-21 17:44:50 -0800

    Steps for task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Put the alphabet in one line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char pr = 'a';
+
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(pr + i);
+	}
+	putchar(\n);
+
+	return (0);
+}
