c549f3bec6c6d82507b97b0f0de0898876e09257
emmanuel <emmanuelpayares.idetp@gmail.com>
2023-02-21 17:02:43 -0800

    Steps for task 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Give you a random number and classify the number
+ *
+ * Return: This time is 0 again
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+	{
+		printf("Last digit of %d is and is greater than 5\n", n);
+	}
+	else if (n < 5 && n > 0)
+	{
+		printf("Last digit of %d is and is less than 6 and not 0\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is and is 0\n");
+	}
+	/* your code goes there */
+	return (0);
+}
